b6c014f2b884d1427000fa8cf82dc5a987291fe1
Igor <chumikov.igor@mail.ru>
2023-08-11 20:34:04 +0700

    Add project

---
+++ SnapshotTest/SnapshotTest/SnapshotTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapshotTestApp.swift
+//  SnapshotTest
+//
+//  Created by Игорь Чумиков on 11.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct SnapshotTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
